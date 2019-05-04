15e2eab8402711327a47ce0fbfe17656a9b92e97
Ivan <ipkozyrin@gmail.com>
2019-05-04 15:52:07 +1000

    add

--- src/StreamingServer.ts
+++ src/StreamingServer.ts
@@ -1,6 +1,5 @@
 const NodeMediaServer = require('node-media-server');
 
-
 // ffmpeg -re -i INPUT_FILE_NAME -c copy -f flv rtmp://localhost/live/STREAM_NAME
 
 // Works only with reconverting a codec
@@ -16,6 +15,8 @@ export default class StreamingServer {
   }
 
   start() {
+    this.startFrmpeg();
+
     const config = {
       logType: 3,
 
@@ -40,6 +41,34 @@ export default class StreamingServer {
 
     var nms = new NodeMediaServer(config)
     nms.run();
+  }
+
+  startFrmpeg() {
+  //   try {
+  //     const ffmpegProc = new ffmpeg('rtsp://admin:admin@192.168.88.33:554/cam/realmonitor?channel=main&subtype=1');
+  //
+  //     ffmpegProc.then(function (video: any) {
+  //       console.log('The video is ready to be processed');
+  //       video
+  //       .setVideoCodec('libx264')
+  //       .addCommand('-f', 'flv')
+  //       .addCommand('-preset', 'superfast')
+  //       .addCommand('-tune', 'zerolatency')
+  //       .setAudioCodec('aac')
+  //       .setAudioFrequency(41)
+  //       .save('rtmp://localhost/live/cam0', function (error: Error, file: any) {
+  //           if (!error)
+  //             console.log('Video file: ' + file);
+  //
+  //         console.log(111111111, file)
+  //         });
+  //     }, function (err: Error) {
+  //       console.log('Error: ' + err);
+  //     });
+  //   } catch (e) {
+  //     console.log(e.code);
+  //     console.log(e.msg);
+  //   }
   }
 
 }
