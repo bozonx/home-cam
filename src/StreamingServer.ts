6e221efb317f84474f498f7aa88b4e89f96eb450
Ivan <ipkozyrin@gmail.com>
2019-05-04 12:48:45 +1000

    add

--- src/StreamingServer.ts
+++ src/StreamingServer.ts
@@ -3,6 +3,13 @@ const NodeMediaServer = require('node-media-server');
 
 // ffmpeg -re -i INPUT_FILE_NAME -c copy -f flv rtmp://localhost/live/STREAM_NAME
 
+// Works only with reconverting a codec
+// ffmpeg -i "rtsp://admin:admin@192.168.88.33:554/cam/realmonitor?channel=main&subtype=1" -c:v libx264 -preset superfast -tune zerolatency -c:a aac -ar 44100 -f flv "rtmp://localhost/live/cam0"
+// ffmpeg -i "rtsp://192.168.88.6:554/user=admin&password=&channel=1&stream=0.sdp" -c:v libx264 -preset superfast -tune zerolatency -c:a aac -ar 44100 -f flv "rtmp://localhost/live/cam0"
+
+// file:
+//ffmpeg -re -i /home/ivan/Downloads/test.mp4 -c:v libx264 -preset superfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost/live/cam0
+
 export default class StreamingServer {
   constructor() {
 
