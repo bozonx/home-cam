76fbca190eccc0a914233ed722b53e49805c196a
Ivan <ipkozyrin@gmail.com>
2019-05-06 11:04:35 +1000

    chore

--- src/RestartedProcess.ts
+++ src/RestartedProcess.ts
@@ -8,13 +8,14 @@ const DEFAULT_RESTART_TIMEOUT = 1000;
 
 export default class RestartedProcess {
   private readonly stdoutEvents = new IndexedEvents<StdHandler>();
-  private readonly stderrEvents = new IndexedEvents<StdHandler>();
+  private readonly errorEvents = new IndexedEvents<StdHandler>();
   private readonly cmd: string;
   private readonly cwd?: string;
   private readonly restartTimeoutMs: number;
   private proc?: SpawnProcess;
   private restartTimeout: any;
-
+  private starting: boolean = false;
+  
 
   constructor(cmd: string, cwd?: string, restartTimeoutMs: number = DEFAULT_RESTART_TIMEOUT) {
     this.cmd = cmd;
@@ -30,7 +31,7 @@ export default class RestartedProcess {
     //this.destroyInstance();
     clearTimeout(this.restartTimeout);
     this.stdoutEvents.removeAll();
-    this.stderrEvents.removeAll();
+    this.errorEvents.removeAll();
     if (this.proc) this.proc.destroy();
     delete this.proc;
   }
@@ -41,57 +42,58 @@ export default class RestartedProcess {
   }
 
   onError(cb: StdHandler) {
-    this.stderrEvents.addListener(cb);
+    this.errorEvents.addListener(cb);
   }
 
 
   private makeInstance = () => {
-    // destroy previous instance if exist
-    if (this.proc) this.proc.destroy();
 
     // make new instance
     this.proc = new SpawnProcess(this.cmd, this.cwd);
     // listen events
     this.proc.onStdOut(this.stdoutEvents.emit);
-    this.proc.onError(this.stderrEvents.emit);
+    this.proc.onError(this.errorEvents.emit);
     this.proc.onClose(this.handleProcClose);
+    this.starting = true;
 
     try {
       this.proc.start();
     }
     catch (err) {
-      // destroy if can't start
-      this.destroy();
       // print error
-      this.stderrEvents.emit(`RestartedProcess: ${err}`);
+      this.errorEvents.emit(`RestartedProcess: ${err}`);
       // fully restart a process
       this.restart();
     }
+
+    this.starting = false;
   }
 
   private handleProcClose = (code: number) => {
-    // reconnect
-
-
-    // TODO: больше не стартовать если задестроенно
+    // do noting if if was called while process is starting
+    if (this.starting) return;
+    
+    // TODO: обработать случай если процесс выполнился и сразу закончился
+    // TODO: больше не стартовать если задестроенно - при дестрое инстанса может вызываться close
 
 
     if (code) {
-      // TODO: use stderrEvents
-      console.error(`RestartedProcess: cmd "${this.cmd}" has been closed with non zero code "${code}"`)
+      this.errorEvents.emit(`RestartedProcess: cmd "${this.cmd}" has been closed with non zero code "${code}"`)
     }
 
-    // restart
     this.restart();
   }
 
   private restart() {
+    // destroy previous instance if exist
+    if (this.proc) this.proc.destroy();
+    // wait to restart
     this.restartTimeout = setTimeout(this.makeInstance, this.restartTimeoutMs);
   }
 
   // private destroyInstance() {
   //   this.stdoutEvents.removeAll();
-  //   this.stderrEvents.removeAll();
+  //   this.errorEvents.removeAll();
   //   if (this.proc) this.proc.destroy();
   //   delete this.proc;
   // }
