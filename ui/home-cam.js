b3c4f059b5023de0cf31186961e4906b3e1a397b
Ivan <ipkozyrin@gmail.com>
2019-05-27 17:04:15 +1000

    add

--- ui/home-cam.js
+++ ui/home-cam.js
@@ -1,25 +1,42 @@
+const IMG_WIDTH = 300;
+const IMG_HEIGHT = 169;
 
-function getElTpl(src) {
-  return `<img src="${src}">`;
+
+function makeImgEl(src, width, height) {
+  const imgEl = document.createElement('input');
+
+  imgEl.setAttribute('src', src);
+  imgEl.setAttribute('width', width);
+  imgEl.setAttribute('height', height);
+
+  return imgEl;
 }
 
-function instantiate(el) {
+function handleImgClick(event) {
 
 }
 
+function instantiate(rootEl, previewSrc) {
+  const imgEl = makeImgEl(previewSrc, IMG_WIDTH, IMG_HEIGHT);
 
-window.place = function (elementSelector) {
+  imgEl.onclick = handleImgClick();
+
+  rootEl.append(imgEl);
+}
+
+
+window.place = function (elementSelector, previewSrc) {
   if (elementSelector.indexOf('.') === 0) {
     const elements = document.getElementsByClassName(elementSelector);
 
     for (let el of elements) {
-      instantiate(el);
+      instantiate(el, previewSrc);
     }
   }
   else if (elementSelector.indexOf('#') === 0) {
     const el = document.getElementById(elementSelector);
 
-    instantiate(el);
+    instantiate(el, previewSrc);
   }
   else {
     throw new Error(`Incorrect selector "${elementSelector}"`);
